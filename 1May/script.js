8a83d18f8d7bf76fd22aae546e4bb3bdaaa5c09d
Komalbaghel08 <baghelkomal512@gmail.com>
2025-05-01 21:04:09 +0530

    new

---
+++ 1May/script.js
@@ -0,0 +1,9 @@
+let year = prompt("Enter a year: ");
+let yearsLeft = 2050 - year;
+console.log(`There are ${yearsLeft} years left until 2050.`); 
+
+// QUE 2
+ let price = parseInt(prompt("enter"));
+let result = (18/100)*price;
+console.log(result) ;
+
--- 25April/practicequestion.js
+++ 25April/practicequestion.js
@@ -8,22 +8,33 @@
 
 
 // Prime or Not
-let n = parseInt(prompt("Enter a number:"));
-let isPrime = true;
+// let n = parseInt(prompt("Enter a number:"));
+// let isPrime = true;
 
-if (n <= 1) {
-  isPrime = false;
+// if (n <= 1) {
+//   isPrime = false;
+// }
+
+// for (let i = 2; i <= Math.sqrt(n); i++) {
+//   if (n % i === 0) {
+//     isPrime = false;
+//     break;
+//   }
+// }
+
+// if (isPrime) {
+//   console.log(n + " is a Prime Number");
+// } else {
+//   console.log(n + " is NOT a Prime Number");
+// }
+
+// WAP to print 1-10 numbers
+for(let i = 1; i<=10; i++){
+    console.log(i);
 }
-
-for (let i = 2; i <= Math.sqrt(n); i++) {
-  if (n % i === 0) {
-    isPrime = false;
-    break;
-  }
-}
-
-if (isPrime) {
-  console.log(n + " is a Prime Number");
-} else {
-  console.log(n + " is NOT a Prime Number");
+// WAP to print even numbers from 1-20
+for(let i = 1; i<=20; i++){
+    if(i%2==0){
+        console.log(i);
+    }
 }
