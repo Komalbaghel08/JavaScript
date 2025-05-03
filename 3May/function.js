7b244c9492ba7b3dafa5bcf690c3cab62ebc8410
Komalbaghel08 <baghelkomal512@gmail.com>
2025-05-03 16:18:06 +0530

    neww

---
+++ 3May/function.js
@@ -0,0 +1,37 @@
+// 4 ways of function
+
+// no returns & no parameter
+function display(){
+    console.log("hello");
+}
+display();
+
+// no returns with parameter
+function add(num1,num2){
+    console.log(num1 + num2);
+}
+add(10,20)
+add(34,67)
+
+// return with parameter
+function multiple(num1,num2){
+    return num1*num2;
+}
+let result = multiple(20,30);
+// alert(result);
+console.log(result);
+
+// return with no parameter
+function show(){
+    return "hello"
+}
+let ans = show();
+console.log(ans);
+
+
+// cube
+function cube(num3){
+    return num3 * num3 * num3
+}
+let d = cube(3)
+console.log(d);
